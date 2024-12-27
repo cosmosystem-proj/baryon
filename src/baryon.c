5c8033d756f9a8b11c168056556419c0dea8f045
Kurt M. Weber <weberk294@gmail.com>
2024-12-26 21:24:29 -0600

    starting source file, license file

---
+++ src/baryon.c
@@ -0,0 +1,9 @@
+/*
+ * Creator: Kurt M. Weber
+ * Created on: 2024-12-26
+ *
+ * This file is part of baryon, a component of the Cosmoverse.
+ * Licensed under Hippocratic License with clauses:
+ * HL3-CL-ECO-EXTR-FFD-MEDIA-MY-SUP-SV-TAL-USTA-XUAR
+ * See file LICENSE for full licensing informatin.
+ */
