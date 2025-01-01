d37f14a189f49db14eebf5621644f6e00973c2b2
Kurt M. Weber <weberk294@gmail.com>
2025-01-01 11:18:06 -0600

    description

---
+++ include/baryon.h
@@ -0,0 +1,13 @@
+/*
+ * Creator: Kurt M. Weber
+ * Created on: 2024-12-26
+ *
+ * This file is part of baryon, a component of the Cosmoverse.
+ * Licensed under Hippocratic License with clauses:
+ * HL3-CL-ECO-EXTR-FFD-MEDIA-MY-SUP-SV-TAL-USTA-XUAR
+ * See file LICENSE for full licensing informatin.
+ */
+
+typedef struct baryon {
+  __UINT64_TYPE__ id;
+} baryon;
