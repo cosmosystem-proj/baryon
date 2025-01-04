385e6226ab87818afca36ab165a4f8a3b4e82cbf
Kurt M. Weber <weberk294@gmail.com>
2025-01-04 01:18:57 -0600

    registerclass.c

---
+++ src/registerclass.c
@@ -0,0 +1,14 @@
+/*
+ * Creator: Kurt M. Weber
+ * Created on: 2025-01-04
+ *
+ * This file is part of baryon, a component of the Cosmoverse.
+ * Licensed under Hippocratic License with clauses:
+ * HL3-CL-ECO-EXTR-FFD-MEDIA-MY-SUP-SV-TAL-USTA-XUAR
+ * See file LICENSE for full licensing informatin.
+ */
+
+#include <quanta/include/types.h>
+
+void baryon_register_class(uint64_t version, qword_t class_id[4], size_t length,
+                           const char *ods) {}
